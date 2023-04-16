ff23992e83786fc82308ec6e25021b542b39c8ff
rewatevijaykumar <rewatevijaykumar@gmail.com>
2023-04-16 20:36:57 +0530

    Hyperparameter_training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,13 +20,20 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
+            rs = RandomizedSearchCV(model, para, cv=3)
+
+            rs.fit(X_train,y_train)
+
+            # model.fit(X_train, y_train)
+            model.set_params(**rs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
